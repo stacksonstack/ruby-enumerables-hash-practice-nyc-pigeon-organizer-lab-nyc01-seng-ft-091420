4a2126ce34040ab1c727aed5ac7b2b28926ec2fa
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-09-07 19:21:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons_by_name = {}
+  data.each do |key, value|
+    value.each do |new_key, new_value|
+      new_key = new_key.to_s
+      new_value.map do |e|
+        if !pigeons_by_name[e]
+          pigeons_by_name[e] = {key => [new_key]}
+        elsif pigeons_by_name[e][key]
+          pigeons_by_name[e] = {key => pigeons_by_name[e][key].push(new_key)}
+        else
+          pigeons_by_name[e].merge!({key => [new_key]})
+        end
+       end 
+    end
+  end
+  pigeons_by_name
 end
